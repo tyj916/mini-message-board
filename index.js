8408a5e8ff224fb17e6919e78b6145df295ab160
Tan Yi Jun <tanyijun6@gmail.com>
2024-09-26 21:37:42 +0800

    Add views

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const path = require('node:path');
 const newRouter = require('./routes/new');
 const app = express();
 const PORT = 3000;
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
   res.send('Hello world!');
